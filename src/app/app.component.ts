828bd4a02b04cfaf8c7039bf24361171caa01988
bunyarit18 <bunyarit1618@rmutsvmail.com>
2021-05-31 12:10:02 +0700

    form partner

---
+++ src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+import { ProgressComponent } from './progress/progress.component';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit, AfterViewInit {
+  testForm = new FormGroup({
+    food: new FormControl('', Validators.required),
+    comment: new FormControl('', Validators.required),
+  });
+  ngOnInit() {}
+
+  goNext(progress: ProgressComponent) {
+    progress.next();
+  }
+
+  onStateChange(event) {
+    console.log(event);
+  }
+
+  ngAfterViewInit() {}
+}
